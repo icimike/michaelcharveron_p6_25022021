a9ebc452fb7543744092aa31c043b8c37a53f3af
Michael <icimike@hotmail.com>
2021-03-05 15:42:30 +0100

    clean up2

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,17 @@
-// import des packages
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-
-
-// utilisation du module 'helmet' pour la sécurité en protégeant l'application de certaines vulnérabilités
-// il sécurise nos requêtes HTTP, sécurise les en-têtes, contrôle la prélecture DNS du navigateur, empêche le détournement de clics
-// et ajoute une protection XSS mineure et protège contre le reniflement de TYPE MIME
-// cross-site scripting, sniffing et clickjacking
 const helmet = require('helmet');
 
-// Ajout des routes pour l'identification & l'authentification
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-// initialisation de la variable app qui contiendra 'express'
 const app = express();
 app.use(cors());
 
-
-// utilisation du module 'dotenv' pour masquer les informations de connexion à la base de données à l'aide de variables d'environnement
 require('dotenv').config();
 
-// Connection à la base de données MongoDB avec la sécurité vers le fichier .env pour cacher le mot de passe
-// L'un des avantages que nous avons à utiliser Mongoose pour gérer notre base de données MongoDB est que nous pouvons implémenter des schémas de données stricts
-// qui permettent de rendre notre application plus robuste
 mongoose.connect(process.env.DB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -35,7 +20,7 @@ mongoose.connect(process.env.DB_URI, {
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Cette partie configure et autorise les requêtes Multi-Origin; définit les Headers & les Methodes
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -43,20 +28,14 @@ app.use((req, res, next) => {
     next();
 });
 
-// Définit la fonction json comme middleware global pour l'application
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Sécuriser Express en définissant divers en-têtes HTTP - https://www.npmjs.com/package/helmet#how-it-works
-// On utilise helmet pour plusieurs raisons notamment la mise en place du X-XSS-Protection afin d'activer le filtre de script intersites(XSS) dans les navigateurs web
 app.use(helmet());
 
-// Enregistrement du routeur pour toutes les demandes effectuées vers /api/sauces.
 app.use('/api/sauces', saucesRoutes);
 
-// Enregistrement du routeur pour toutes les demandes effectuées vers /api/auth.
 app.use('/api/auth', userRoutes);
 
-// Permet d'exporter l'application créer (en l'occurence, elle devient accesible pour serveur.js)
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
-// import du package HTTP natif de Node (permet de créer un serveur)
 const http = require('http');
-
 const app = require('./app');
 
-// la fonction normalizePort renvoie un port valide, qu'il soit de type numérique ou chaîne
 const normalizePort = val => {
     const port = parseInt(val,10);
     if (isNaN(port)) {
         return val;
     }
-
     if (port >= 0) {
         return port;
     }
@@ -18,12 +14,10 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// la fonction errorHandler recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur.
 const errorHandler = error => {
     if (error.syscall !== 'listen') {
         throw error;
     }
-
     const adress = server.address();
     const bind = typeof adress === 'string' ? 'pipe' + adress : 'port: ' + port;
     switch (error.code) {
@@ -40,10 +34,8 @@ const errorHandler = error => {
     }
 };
 
-// creation du serveur et Appel de l'application 'Express' pour envoie des arguments
 const server = http.createServer(app);
 
-// un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
 server.on('error', errorHandler);
 server.on('listening', ()=>{
     const adress = server.address();
